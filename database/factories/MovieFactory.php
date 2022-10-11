234c1ea0607cfadda6d5a022f73c3382277758be
Chyngyz Sydykov <93588104+turbine-chyngyz-sydykov@users.noreply.github.com>
2022-10-11 22:17:23 +0600

    chika-2 movie realted code (#6)

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->text(50),
+            'description' => fake()->sentence(10),
+            'year' => fake()->year(),
+            'duration' => fake()->time()
+        ];
+    }
+}
