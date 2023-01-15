2febdced383ed07be1c10be8f4113afbb6247ead
Chyngyz Sydykov <93588104+turbine-chyngyz-sydykov@users.noreply.github.com>
2023-01-16 00:54:43 +0600

    chika movie resource tests (#38)
    * chika movie resource tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
+<p><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
-<p align="center">
+<p>
 <a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
@@ -44,6 +44,9 @@ Laravel has the most extensive and thorough [documentation](https://laravel.com/
 
 If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 2000 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
 
-## Code of Conduct
+## Code examples
 
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
+```sail composer dump-autoload
+sail artisan cache:clear
+sail artisan config:clear
+sail artisan view:clear```
--- tests/Feature/MoviePagesTest.php
+++ tests/Feature/MoviePagesTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Movie;
 use Tests\TestCase;
 
 class MoviePagesTest extends TestCase
@@ -9,41 +10,40 @@ class MoviePagesTest extends TestCase
     /**
      * @return void
      */
-    public function testIndexPageToReturnSuccessfulResponse(): void
+    public function testMovieIndexPage(): void
     {
 
-        $response = $this->get('/movie');
+        $response = $this->get(route('movie.index'));
 
         $response->assertStatus(200);
     }
     /**
      * @return void
      */
-    public function testCreatePageToReturnSuccessfulResponse(): void
+    public function testMovieCreatePage(): void
     {
-
-        $response = $this->get('/movie/create');
-
+        $response = $this->get(route('movie.create'));
         $response->assertStatus(200);
     }
 
     /**
      * @return void
      */
-    public function testInvalidMovieCreationData()
+    public function testMovieCreateWithInvalidData()
     {
-        $response = $this->post('/movie/store', [
-            'title' => 'Test User',
+        $response = $this->post(route('movie.store'), [
+            'title' => 'Test Movie',
         ]);
-        $this->assertEquals(405, $response->getStatusCode());
+        $response->assertSessionHasErrors(['description', 'year', 'duration']);
+        $this->assertEquals(302, $response->getStatusCode());
     }
 
     /**
      * @return void
      */
-    public function testNewMovieCanBeCreated()
+    public function testNewMovieStorePage()
     {
-        $response = $this->post('/movie/store', [
+        $response = $this->post(route('movie.store'), [
             'title' => 'Test Movie',
             'description' => 'Test Movie description',
             'year' => '2023',
@@ -52,6 +52,59 @@ class MoviePagesTest extends TestCase
             'country-id' => [2],
         ]);
 
-        $this->assertEquals(405, $response->getStatusCode());
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHasAll(['success']);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $this->assertDatabaseHas('movies', ['title' => 'Test Movie']);
+    }
+
+    public function testMovieEditPage()
+    {
+        $movie = Movie::factory()->create(['title' => 'test movie edit']);
+
+        $response = $this->get(route('movie.edit', $movie->id));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('movie.edit');
+        $response->assertViewHas('movie', $movie);
+    }
+
+    public function testMovieUpdatePage()
+    {
+        $movie = Movie::factory()->create(['title' => 'test movie update']);
+        $data = [
+            'title' => 'test movie updated',
+            'description' => 'Movie 2 description',
+            'year' => '2023',
+            'duration' => '2:30:00',
+            'genre-id' => [1],
+            'country-id' => [2],
+        ];
+        $response = $this->put(route('movie.update', $movie->id), $data);
+
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHasAll(['success']);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $this->assertDatabaseHas('movies', ['title' => 'test movie updated']);
+    }
+
+    public function testMovieDeleteOperation()
+    {
+        $movie = Movie::factory()->create([
+            'title' => 'test movie delete',
+            'description' => 'Movie 2 description',
+            'year' => '2023',
+            'duration' => '2:30:00',
+        ]);
+
+        $response = $this->delete(route('movie.destroy', $movie->id));
+
+        $response->assertSessionHasNoErrors();
+        $response->assertSessionHasAll(['success']);
+        $this->assertEquals(302, $response->getStatusCode());
+
+        $this->assertDatabaseMissing('movies', ['title' => 'test movie delete']);
     }
 }
