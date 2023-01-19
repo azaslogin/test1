045cdee3da3195b835f89a712a00f709c2ca4ea9
Chyngyz Sydykov <chyngyz.sydykov@turbinekreuzberg.com>
2023-01-19 13:50:10 +0800

    chika installation of breeze

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('content')
+    <div class="w-full flex flex-col">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-6 text-gray-900">
+                {{ __("You're logged in!") }}
+            </div>
+            </div>
+        </div>
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\GenreController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\MovieByGenreController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -42,3 +43,15 @@ Route::get('moviebygenre/index/{id}', [MovieByGenreController::class, 'index'])-
 Route::get('/user/{id}', function ($id) {
     return 'User ' . $id;
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
